2fd50264410c2accf2ba5c26d20650fc0e5f7eed
Akhil Ravipati <akhil.ravipati.o@gmail.com>
2020-03-15 14:55:05 +0530

    Remove certain sensitive bits of inforamtion from the sql scripts

--- admin/new1.php
+++ admin/new1.php
@@ -1,6 +1,6 @@
 <?php
     require("../connect_db.php");
-	$pass_hash=password_hash("akhil",PASSWORD_DEFAULT);
+	$pass_hash=password_hash("nonce",PASSWORD_DEFAULT);
 	
 	$sql="UPDATE login SET password='".$pass_hash."',username='ieee_admin' WHERE username='admin';";
 	$conn->query($sql);
